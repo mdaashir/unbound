3080f4e05c2679d38b759b2da04503410cd025ef
mdaashir <22pt17@psgtech.ac.in>
2025-02-19 13:52:38 +0530

    Add FastAPI application with basic endpoints and .gitignore file

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+@app.post("/login")
+async def login(username: str, password: str):
+    return {"username": username, "password": password}
